591b011c61cc241e8e23718f252fcaa5cc4167b7
sanderheieren <sanheieren@gmail.com>
2021-01-26 23:45:19 +0100

    flower generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import React, {useRef, useEffect} from 'react'
+import Canvas from './Canvas'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Hello</h1>
+      <h1>Hello, I'm Sander</h1>
       </header>
+      <main>
+        <p className="sub-text">I'm huge fan of the Fibonacci Sequence <br/>
+        Take a look at my algorithmic flower generator
+        </p>
+      </main>
+       <Canvas />
     </div>
   );
 }
